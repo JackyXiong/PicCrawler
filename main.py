db6189e3331ba922c8660429ac4c0c8515766437
熊杰 <xiongjiekobe@gmail.com>
2014-07-03 16:54:43 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+'''
+'''
+
+from picCrawler import Crawler
+from urlFetcher import Fetch
+from options import parser
+
+def main():
+	args = parser.parse_args()
+	print args.url,'------'
+
+	Fetcher =Fetch(args.url, args.threadNum, args.limit)
+	Fetcher.start()
+
+	PicCrawler = Crawler(args.threadNum, args.path, args.limit)
+	PicCrawler.start()
+
+if __name__ == '__main__':
+	main()
