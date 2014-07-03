9df75ad9587d23d867892038256e770c7f691989
熊杰 <xiongjiekobe@gmail.com>
2014-07-03 16:51:55 +0800

    Create picFile.py

---
+++ picFile.py
@@ -0,0 +1,26 @@
+#coding:utf-8
+'''
+picFile.py
+存取 URL 链接的文件操作
+'''
+
+class PicFile(object):
+	'''way 表示读取模式，a or r'''
+	def __init__(self, filename,way):
+		super(PicFile, self).__init__()
+		global fileHandle
+		fileHandle = open(filename+'.txt',way)
+
+	'''对每一个 URL 进行存储'''
+	def saveData(self,url):
+		fileHandle.write(url+'\n')
+
+	'''取出一个 URL'''
+	def getData(self):
+		return fileHandle.readline()
+
+	def close(self):
+		fileHandle.close()
+
+		
+	
