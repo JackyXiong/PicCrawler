3411903e40ea07fb98f2ffcf4fe94de11f31712e
熊杰 <xiongjiekobe@gmail.com>
2014-07-07 12:54:22 +0800

    Update picFile.py

--- picFile.py
+++ picFile.py
@@ -1,26 +1,23 @@
 #coding:utf-8
 '''
-picFile.py
 存取 URL 链接的文件操作
 '''
 
 class PicFile(object):
-	'''way 表示读取模式，a or r'''
-	def __init__(self, filename,way):
-		super(PicFile, self).__init__()
-		global fileHandle
-		fileHandle = open(filename+'.txt',way)
+    '''way 表示读取模式，a or r'''
+    def __init__(self, filename,way):
+        super(PicFile, self).__init__()
+        global fileHandle
+        fileHandle = open(filename+'.txt',way)
+#        print '打开'
 
-	'''对每一个 URL 进行存储'''
-	def saveData(self,url):
-		fileHandle.write(url+'\n')
+    '''对每一个 URL 进行存储'''
+    def saveData(self,url):
+        fileHandle.write(url+'\n')
 
-	'''取出一个 URL'''
-	def getData(self):
-		return fileHandle.readline()
+    '''取出每一个 URL'''
+    def getData(self):
+        return fileHandle.readline()
 
-	def close(self):
-		fileHandle.close()
-
-		
-	
+    def close(self):
+        fileHandle.close()
