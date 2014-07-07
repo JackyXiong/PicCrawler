4dbce97ff0aca3f89e52aceae37f3255168a937a
熊杰 <xiongjiekobe@gmail.com>
2014-07-07 12:51:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 #coding:utf-8
 '''
 '''
@@ -7,14 +8,13 @@ from urlFetcher import Fetch
 from options import parser
 
 def main():
-	args = parser.parse_args()
-	print args.url,'------'
+    args = parser.parse_args()
 
-	Fetcher =Fetch(args.url, args.threadNum, args.limit)
-	Fetcher.start()
+    Fetcher =Fetch(args.url, args.threadNum, args.limit)
+    Fetcher.start()
 
-	PicCrawler = Crawler(args.threadNum, args.path, args.limit)
-	PicCrawler.start()
+    PicCrawler = Crawler(args.threadNum, args.path, args.limit)
+    PicCrawler.start()
 
 if __name__ == '__main__':
-	main()
+    main()
